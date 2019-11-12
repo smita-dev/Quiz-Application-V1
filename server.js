b1e0dcac9f6e10598ee7ee1210f65a3135432e78
smita patil <smitakpatil26@gmail.com>
2019-11-12 21:29:20 +0530

    removed comments

--- server.js
+++ server.js
@@ -21,37 +21,3 @@ mongo.connect(db.url, { useNewUrlParser: true, useUnifiedTopology: true }, (err,
         console.log('connected to db');
    });
 });
-
-// app.use(session({
-//     cookie: { secure: true }
-//   }))
-// passport.use(new LocalStrategy(
-//   function(username, password, done) {
-//     User.findOne({ username: username }, function (err, user) {
-//       if (err) { return done(err); }
-//       if (!user) {
-//         return done(null, false, { message: 'Incorrect username.' });
-//       }
-//       if (!user.validPassword(password)) {
-//         return done(null, false, { message: 'Incorrect password.' });
-//       }
-//       return done(null, user);
-//     });
-//   }
-// ));
-
-// passport.use(new LocalStrategy(
-//     function(username, password, done) {
-//       User.findOne({ username: username }, function (err, user) {
-//         if (err) { return done(err); }
-//         if (!user) {
-//           return done(null, false, { message: 'Incorrect username.' });
-//         }
-//         if (!user.validPassword(password)) {
-//           return done(null, false, { message: 'Incorrect password.' });
-//         }
-//         return done(null, user);
-//       });
-//     }
-//   ));
-
