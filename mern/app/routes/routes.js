451ac8196343fd1543a24f0c57c9d8ae25394618
smita patil <smitakpatil26@gmail.com>
2019-11-15 21:33:30 +0530

    removed user route

--- mern/app/routes/routes.js
+++ mern/app/routes/routes.js
@@ -1,24 +1,10 @@
 module.exports = (app, db) => {
-    
-         app.post("/", (req, res) => {
-            console.log(req.body.Username);
-            console.log(req.body.Password);
-            const note= req.body;
-            db.collection("user").insertOne(note, function(err, res) {
-                if (err) throw err;
-                console.log("1 document inserted");
-              });
-        });
-        app.get("/", (req, res) => {
-        
+        app.get("/", (req, res) => {        
             db.collection("test").find({}).toArray(function(err, result) {
-            if (err) throw err;
-            res.status(200).send(result);
-            console.log(result);
+                if (err) throw err;
+                res.status(200).send(result);
+                console.log(result);
             });
 
         });
-        // app.get("/login",(req,res)=>{
-        //     db.collection("test").findOne{},(err,result)
-        // })
  };
