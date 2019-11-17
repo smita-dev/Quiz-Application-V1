70cb8eb77ae9a420a3e5bb3c1e02099d2457f97a
smita patil <smitakpatil26@gmail.com>
2019-11-17 22:43:28 +0530

    updated path

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongo = require('mongodb').MongoClient;
-const db = require('./mern/config/config');
 const bodyParser = require('body-parser');
 const cors=require('cors');
 const port = process.env.PORT || 8000;
@@ -9,13 +8,15 @@ app.use(cors());
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
+const url="mongodb+srv://dbUser:q9twDpjE06O9oTxd@cluster0-uz0v7.mongodb.net/test?retryWrites=true&w=majority";
+
 //connecting to mongodb database
-mongo.connect(db.url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) =>
+mongo.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) =>
  {
    if (err)
         return console.log(err);
         const database = db.db('quiz');
-        require('./mern/app/routes')(app, database);
+        require('./app/routes')(app, database);
         app.listen(port, () => {
         console.log('connected to db');
    });
